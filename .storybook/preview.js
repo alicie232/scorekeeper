54ee2a6bf8aa58bfa352e0c240c4cbc77dd1eeee
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-01 13:55:06 +0200

    Prepage styled-components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import GlobalStyle from "../src/GlobalStyle";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +8,13 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,5 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
 body {
-  margin: 0;
+  margin: 0 auto;
+  max-width: 600px;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
@@ -11,3 +15,4 @@ code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 }
+`;
