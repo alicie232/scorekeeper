9944772e97bdbeffd51b4091a0c7f379482a7dab
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-01 14:31:47 +0200

    Add Player component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import Player from "./components/Player/Player.js";
+
+const players = [
+  {
+    name: "John",
+    score: 15,
+  },
+  {
+    name: "Jane",
+    score: 15,
+  },
+  {
+    name: "Jim",
+    score: 15,
+  },
+];
+
 function App() {
-  return <h1>Scorekeeper</h1>;
+  return (
+    <>
+      <heading>
+        <h1>Game</h1>
+      </heading>
+      <main>
+        {players.map((player) => (
+          <Player name={player.name} score={player.score} />
+        ))}
+      </main>
+    </>
+  );
 }
 
 export default App;
